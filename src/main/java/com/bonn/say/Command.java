212196d8ee0ce7f7b0fcca2bbbfd8b70e47e4748
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2022-06-16 19:58:17 -0400

    Initial commit

---
+++ src/main/java/com/bonn/say/Command.java
@@ -0,0 +1,32 @@
+package com.bonn.say;
+
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.annotation.Nonnull;
+import java.util.Objects;
+
+public class Command extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@Nonnull SlashCommandInteractionEvent event) {
+        if (!event.getName().equals("say")) return;
+        switch (Objects.requireNonNull(event.getSubcommandName())) {
+            case "text" -> {
+                TextChannel channel = event.getTextChannel();
+                if (event.getOption("channel") != null)
+                    channel = Objects.requireNonNull(event.getOption("channel")).getAsTextChannel();
+                if (channel != null) {
+                    event.reply("Sending message in %s".formatted(channel.getAsMention())).setEphemeral(true).queue();
+                    channel.sendMessage(Objects.requireNonNull(event.getOption("message")).getAsString()).queue();
+                } else {
+                    event.reply("That is not a valid text channel!").setEphemeral(true).queue();
+                }
+            }
+            case "embed" -> {
+                event.reply("This is not *yet* implemented").setEphemeral(true).queue();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/bonn/say/Main.java
@@ -0,0 +1,50 @@
+package com.bonn.say;
+
+import com.bonn2.Bot;
+import com.bonn2.modules.Module;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+public class Main extends Module {
+
+    @Override
+    public void registerSettings() {
+
+    }
+
+    @Override
+    public void load() {
+        Bot.jda.addEventListener(new Command());
+    }
+
+    @Override
+    public CommandData[] getCommands() {
+        return new CommandData[] {
+                Commands.slash(
+                        "say",
+                        "Make the bot say something."
+                ).addSubcommands(
+                        new SubcommandData(
+                                "text",
+                                "Make the bot send a regular message."
+                        ).addOption(
+                                OptionType.STRING,
+                                "message",
+                                "The message to send.",
+                                true
+                        ).addOption(
+                                OptionType.CHANNEL,
+                                "channel",
+                                "The channel to send the message in.",
+                                false
+                        ),
+                        new SubcommandData(
+                                "embed",
+                                "Make the bot send an embed"
+                        )
+                ).setDefaultEnabled(false)
+        };
+    }
+}
